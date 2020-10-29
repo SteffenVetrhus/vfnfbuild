bac842d8179f344be2b33b69b96b0cd3a0d45215
Steffen <steffenvetrhus1@gmail.com>
2020-10-29 20:41:54 +0100

    First build

---
+++ build/precache-manifest.52b428c13daed982daafae8bf9397fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe05b8b3e919a3c32b9f97947477f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee5cc6dbe61166e25577",
+    "url": "/static/css/main.6ba0d308.chunk.css"
+  },
+  {
+    "revision": "39f65adbdbdcfd3cb8d0",
+    "url": "/static/js/2.f9ed631a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f9ed631a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5cc6dbe61166e25577",
+    "url": "/static/js/main.47a53c42.chunk.js"
+  },
+  {
+    "revision": "5f37b421f4de8f1f149e",
+    "url": "/static/js/runtime-main.172e1990.js"
+  }
+]);
